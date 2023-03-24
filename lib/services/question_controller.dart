dc4b3d5eadb84b665f8a01a6cd1f1a060595e826
justin <justinleddet@gmail.com>
2023-03-24 17:23:20 +0900

    fixed names

--- lib/services/question_controller.dart
+++ lib/services/question_controller.dart
@@ -8,63 +8,19 @@ class QuestionController extends GetxController
     
 
     late PageController _pageController;
-    PageController get pageController => this._pageController;
+    PageController get pageController => _pageController;
 
     bool _isAnswered = false;
-    bool get isAnswered => this._isAnswered;
+    bool get isAnswered => _isAnswered;
 
     late int _correctAns; 
-    int get correctAns => this._correctAns;
+    int get correctAns => _correctAns;
 
     late int _selectedAns;
-    int get selectedAns => this._selectedAns;
+    int get selectedAns => _selectedAns;
 
     int _numOfCorrectAns = 0;
-    int get numOfCorrectAns => this._numOfCorrectAns;
-
-
-    /* void nextQuestion(int index , int total_length) {
-      if ( index != total_length) {
-        _isAnswered = false;
-        _pageController.nextPage(
-            duration: Duration(milliseconds: 250), curve: Curves.ease);
-      } 
-      else {
-        // Get package provide us simple way to naviigate another page
-        Get.to(ScoreScreen(total_length));
-      }
-    } */
-
-
-    /* void checkAns(QueryDocumentSnapshot question, int selectedIndex, int length) {
-      // because once user press any option then it will run
-      _isAnswered = true;
-      _correctAns = question[selectedIndex]['option1'];
-      _selectedAns = selectedIndex;
-
-      if (_correctAns == _selectedAns) _numOfCorrectAns++;
-
-
-      // Once user select an ans after 3s it will go to the next qn
-      Future.delayed(Duration(seconds: 3), () {
-        nextQuestion(selectedIndex, length);
-      }
-    ); 
-  }*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    int get numOfCorrectAns => _numOfCorrectAns;
 
 
 }
